bc1ae938f6d3ca583d5ec03a398c97920b39997f
Gauri <gaurishinde2518@gmail.com>
2022-01-15 17:56:15 +0530

    second commit for search and login page

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -5,4 +5,8 @@ public class LoginPage {
     public void LoginPageFunction(){
         System.out.println("loginpagefunction");
     }
+
+    public void LoginPageFunction1(){
+        System.out.println("loginpagefunction1");
+    }
 }
