043e9a35d2ac4cbcb526d76fee280a0114fd63cd
Gauri <gaurishinde2518@gmail.com>
2022-01-16 21:31:58 +0530

    commit for search page: master 1st time

--- src/main/java/com/qa/pages/searchPage.java
+++ src/main/java/com/qa/pages/searchPage.java
@@ -3,6 +3,14 @@ package com.qa.pages;
 public class searchPage {
 
     public void searchPage(){
-        System.out.println("search page method updated");
+        System.out.println("search page method updated: UAT update");
+    }
+
+    public void searchPage1(){
+        System.out.println("search page method updated: UAT1 update");
+    }
+
+    public void searchPage2(){
+        System.out.println("search page method updated: master update");
     }
 }
