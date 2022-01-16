742ec4148b5005723065f00e6777de382b2c4579
Gauri <gaurishinde2518@gmail.com>
2022-01-16 21:30:05 +0530

    commit for search page:UAT 2nd time

--- src/main/java/com/qa/pages/searchPage.java
+++ src/main/java/com/qa/pages/searchPage.java
@@ -5,4 +5,8 @@ public class searchPage {
     public void searchPage(){
         System.out.println("search page method updated: UAT update");
     }
+
+    public void searchPage1(){
+        System.out.println("search page method updated: UAT1 update");
+    }
 }
